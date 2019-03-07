829bc52382d2d0101c58f78b7fe24bdacf0e0b0c
SCOPUS\jpalmeida <jpalmeida@scopus.com.br>
2019-03-07 11:15:03 -0300

    Downgrade java 1.8 para 1.7

--- src/main/java/br/com/unipass/novamed/ServletInitializer.java
+++ src/main/java/br/com/unipass/novamed/ServletInitializer.java
@@ -1,7 +1,7 @@
 package br.com.unipass.novamed;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
